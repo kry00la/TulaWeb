b2464f2084064f83ebf927cdefd2adc0ea899146
ludapdap <ludapdap@dynamicobjx.com>
2012-09-29 09:53:13 -0700

    myfirst

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tula::Application.config.secret_token = '9e0c5665487f298f619fba0defa0bd1d54c0b5839c888a57ab028d0368f1e1ed355988c777ff6c9125923cb5107d98b75eba8f8e3d5c17e58736e49ac2c6b040'
